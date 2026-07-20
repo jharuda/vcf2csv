c2a83261a091ac72dd5e7ed1cd11442c460ea9d3
Jakub Haruda <jharuda@users.noreply.github.com>
2026-07-20 21:30:57 +0200

    Tests: Add test number 4 for skipped fields

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -298,14 +298,60 @@ test_3() {
 }
 
 
+test_4() {
+    id_test="4"
+    header_start "$id_test"
+    file="${DATA_DIR}/contacts_3_version2-1.vcf"
+    test_folder="test_${id_test}"
+
+    print_info "Check for printed result with skipped field"
+    output_file_1="${TMP_DIR}/test${id_test}_print_skip_field.out"
+    params_1="-i ${file} --skip-fields FN"
+    print_params "$params_1"
+    $PROGRAM_EXEC $params_1 > "${output_file_1}"
+    check_test_result "${output_file_1}" "${DATA_DIR}/${test_folder}/test_print.out" $? 0
+
+    print_info "Check for printed result with skipped field"
+    output_file_2="${TMP_DIR}/test${id_test}_print_skip_field.out"
+    params_2="-i ${file} -s FN"
+    print_params "$params_2"
+    $PROGRAM_EXEC $params_2 > "${output_file_2}"
+    check_test_result "${output_file_2}" "${DATA_DIR}/${test_folder}/test_print.out" $? 0
+
+    print_info "Check for output JSON with skipped field"
+    output_file_3="${TMP_DIR}/test${id_test}_skip_field.json"
+    params_3="-i ${file} --json ${output_file_3} -s FN"
+    print_params "$params_3"
+    $PROGRAM_EXEC $params_3
+    check_test_result "${output_file_3}" "${DATA_DIR}/${test_folder}/test.json" $? 0
+
+    print_info "Check for output CSV with skipped field"
+    output_file_4="${TMP_DIR}/test${id_test}_skip_field.csv"
+    params_4="-i ${file} --output ${output_file_4} -s FN"
+    print_params "$params_4"
+    $PROGRAM_EXEC $params_4
+    check_test_result "${output_file_4}" "${DATA_DIR}/${test_folder}/test.csv" $? 0
+
+    print_info "Check for output nice print with skipped field"
+    output_file_5="${TMP_DIR}/test${id_test}_nice_print_skip_field.out"
+    params_5="-i ${file} --nice -s FN"
+    print_params "$params_5"
+    $PROGRAM_EXEC $params_5 > "${output_file_5}"
+    check_test_result "${output_file_5}" "${DATA_DIR}/${test_folder}/test_nice_print.out" $? 0
+
+    footer_end
+}
+
+
 main() {
     TMP_DIR="$( mktemp -d )"
     failed_tests=0
     succ_tests=0
 
-    test_1
-    test_2
-    test_3
+    test_1 # one record
+    test_2 # two records
+    test_3 # three resords
+    test_4 # skip field and three records
 
     echo "=========================="
     echo -e "${BHF_COLOR_RED}FAILED TESTS:${BHF_COLOR_DEFAULT}    $failed_tests"
